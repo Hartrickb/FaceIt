ce00f9fcf50d9f7d718d3be1ef93b4c658ecad18
Hartrickb <Hartrickb@users.noreply.github.com>
2017-05-20 21:47:52 -0400

    fix: face does not hit edges due to scale added

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class FaceView: UIView {
-
+    
+    var scale: CGFloat = 0.9
+    
     override func draw(_ rect: CGRect) {
-        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2 * scale
         let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         path.lineWidth = 5.0
