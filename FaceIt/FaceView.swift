279fa4897ec8bbd591a3118c44a317df43647601
Hartrickb <Hartrickb@users.noreply.github.com>
2017-05-21 13:32:28 -0400

    feat: added a mouth that can smile and frown

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -14,6 +14,8 @@ class FaceView: UIView {
     
     var eyesOpen = true
     
+    var mouthCurvature: Double = -10.5 // 1.0 is full smile and -1.0 is full frown
+    
     private var skullRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
     }
@@ -53,6 +55,31 @@ class FaceView: UIView {
         
     }
     
+    private func pathForMouth() -> UIBezierPath {
+        
+        let mouthWidth = skullRadius / Ratios.skullRadiusToMouthWidth
+        let mouthHeight = skullRadius / Ratios.skullRadiusToMouthHeight
+        let mouthOffset = skullRadius / Ratios.skullRadiusToMouthOffset
+        
+        let mouthRect = CGRect(x: skullCenter.x - (mouthWidth / 2), y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
+        
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.midY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.midY)
+        
+        let cp1 = CGPoint(x: start.x + mouthRect.width / 3, y: start.y + smileOffset)
+        let cp2 = CGPoint(x: end.x - mouthRect.width / 3, y: start.y + smileOffset)
+        
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        
+        path.lineWidth = 5.0
+        return path
+        
+    }
+    
     private func pathForSkull() -> UIBezierPath {
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         path.lineWidth = 5.0
@@ -64,6 +91,7 @@ class FaceView: UIView {
         pathForSkull().stroke()
         pathForEye(.left).stroke()
         pathForEye(.right).stroke()
+        pathForMouth().stroke()
     }
     
     private struct Ratios {
