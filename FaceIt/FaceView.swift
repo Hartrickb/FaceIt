7782426a8050cc3e9adaf47ae29135beb2e812e1
Hartrickb <Hartrickb@users.noreply.github.com>
2017-05-20 21:44:55 -0400

    add: draws the skull of the face

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,22 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Bennett Hartrick on 5/20/17.
+//  Copyright © 2017 Bennett. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.blue.set()
+        path.stroke()
+    }
+
+}
