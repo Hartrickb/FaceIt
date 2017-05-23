5b5c2fa4e4bb2e7d696c1129b6efb1bfb6fd633e
Hartrickb <Hartrickb@users.noreply.github.com>
2017-05-23 10:43:11 -0400

    feat: added a split view controller to pick emotions for the face

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by Bennett Hartrick on 5/23/17.
+//  Copyright © 2017 Bennett. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationViewController = segue.destination
+        if let faceViewController = destinationViewController as? FaceViewController {
+            if let identifier = segue.identifier {
+                if let expression = emotionalFaces[identifier] {
+                    faceViewController.expression = expression
+                }
+            }
+        }
+    }
+    
+    private let emotionalFaces: Dictionary<String, FacialExpression> = [
+        "sad": FacialExpression(eyes: .closed, mouth: .frown),
+        "happy": FacialExpression(eyes: .open, mouth: .smile),
+        "worried": FacialExpression(eyes: .open, mouth: .smirk)
+    ]
+    
+}
